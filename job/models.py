b1967af5c51aee10010f7ed8f00bebbc132b260c
karim-raouf <karimraoufm@gmail.com>
2023-06-30 00:55:17 +0200

    added alot

--- job/models.py
+++ job/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
 
-
-
 JOB_TYPE = (
     ('Full Time','Full Time'),
     ('Part Time','Part Time'),
     )
+
+def image_upload(instance , filename ):
+    imagename , extension = filename.split('.')
+    return "jobs/%s.%s"%(instance.id , extension)
+
 
 
 
@@ -24,7 +27,7 @@ class Job(models.Model):    #table
     salary = models.IntegerField(default = 0)
     category = models.ForeignKey('Category', on_delete = models.CASCADE)
     experience = models.IntegerField(default=1)
-    
+    image = models.ImageField(upload_to = image_upload)
     
     def __str__(self):
         return self.title
--- job/urls.py
+++ job/urls.py
@@ -3,5 +3,5 @@ from django.urls import path , include
 
 urlpatterns = [
     path('', views.job_list),
-    path('<int : id>', views.job_details),
+    path('<int:id>', views.job_details),
 ]

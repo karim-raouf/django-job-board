884d10a66072c703f5f591e930de9a2b68aeee5d
karim-raouf <karimraoufm@gmail.com>
2023-07-01 18:15:35 +0200

    change job details

--- job/urls.py
+++ job/urls.py
@@ -1,7 +1,9 @@
 from . import views
 from django.urls import path , include
 
+app_name = 'job'
+
 urlpatterns = [
     path('', views.job_list),
-    path('<int:id>', views.job_details),
+    path('<int:id>', views.job_details , name = 'job_detail'),
 ]

311b6f70c23cc134c70cee93126639fb853ad47e
karim-raouf <karimraoufm@gmail.com>
2023-07-07 23:41:01 +0200

    all done and some of api

---
+++ job/api.py
@@ -0,0 +1,20 @@
+from .models import Job
+from .serializers import JobSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+
+@api_view(['GET'])
+def job_list_api(request):
+    all_jobs = Job.objects.all()
+    data = JobSerializer(all_jobs , many = True).data
+    return Response({'data' : data})
+
+
+@api_view(['GET'])
+def job_detail_api(request , id):
+    jobs_detail = Job.objects.get(id = id)
+    data = JobSerializer(jobs_detail).data
+    return Response({'data' : data})
+    
--- job/urls.py
+++ job/urls.py
@@ -1,4 +1,4 @@
-from . import views
+from . import views , api
 from django.urls import path , include
 
 app_name = 'job'
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.job_list , name = 'job_list'),
     path('add', views.add_job , name = 'add_job'),
     path('<str:slug>', views.job_details , name = 'job_detail'),
-    
+    path('api/jobs', api.job_list_api , name = 'job_list_api'),
+    path('api/jobs/<int:id>', api.job_detail_api , name = 'job_detail_api'),
 ]
